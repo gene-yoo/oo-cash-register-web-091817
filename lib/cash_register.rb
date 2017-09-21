832a94b48e432fe13ad0452e07ae432f95755581
gene-yoo <gene.y.yoo@gmail.com>
2017-09-21 13:53:22 -0400

    Create CashRegister class and respective methods

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
+  attr_accessor :total, :discount, :items, :last_item
 
+  def initialize(discount=nil)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quant = 1)
+    @last_item = [title, price, quant]
+    quant.times {@items << @last_item[0]}
+    self.total += @last_item[1] * @last_item[2]
+  end
+
+  def apply_discount
+    if self.discount.nil?
+      "There is no discount to apply."
+    else
+      self.total = (self.total * (100 - self.discount.to_f) / 100).to_i
+      "After the discount, the total comes to $#{self.total}."
+    end
+  end
+
+  def void_last_transaction
+    @last_item[2].times {@items.pop}
+    self.total -= @last_item[1] * @last_item[2]
+  end
+end
